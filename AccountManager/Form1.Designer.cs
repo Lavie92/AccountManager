49a4845f7f33e8281709a51e1dc0b10894c90d0d
Lavie <paintjinx1@gmail.com>
2022-07-26 01:32:03 +0700

    Add project files.

---
+++ AccountManager/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace AccountManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblTitle = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblAccountNumber = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblRemainder = new System.Windows.Forms.Label();
+            this.txtAccountNumber = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtRemainder = new System.Windows.Forms.TextBox();
+            this.lvwInformation = new System.Windows.Forms.ListView();
+            this.colNumber = new System.Windows.Forms.ColumnHeader();
+            this.colAccountNumber = new System.Windows.Forms.ColumnHeader();
+            this.colCustomerName = new System.Windows.Forms.ColumnHeader();
+            this.colAddress = new System.Windows.Forms.ColumnHeader();
+            this.colRemainder = new System.Windows.Forms.ColumnHeader();
+            this.btnAddUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tblTitle.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblTitle
+            // 
+            this.tblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblTitle.ColumnCount = 1;
+            this.tblTitle.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblTitle.Controls.Add(this.lblTitle, 0, 0);
+            this.tblTitle.Location = new System.Drawing.Point(12, 21);
+            this.tblTitle.Name = "tblTitle";
+            this.tblTitle.RowCount = 1;
+            this.tblTitle.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblTitle.Size = new System.Drawing.Size(889, 117);
+            this.tblTitle.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblTitle.Location = new System.Drawing.Point(3, 42);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(883, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "QUẢN LÝ THÔNG TIN TÀI KHOẢN";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Controls.Add(this.lblAccountNumber, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblCustomerName, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblAddress, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblRemainder, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtAccountNumber, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtCustomerName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtAddress, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtRemainder, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(46, 148);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(650, 119);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lblAccountNumber
+            // 
+            this.lblAccountNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAccountNumber.AutoSize = true;
+            this.lblAccountNumber.Location = new System.Drawing.Point(3, 7);
+            this.lblAccountNumber.Name = "lblAccountNumber";
+            this.lblAccountNumber.Size = new System.Drawing.Size(189, 15);
+            this.lblAccountNumber.TabIndex = 0;
+            this.lblAccountNumber.Text = "Số tài khoản";
+            this.lblAccountNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(3, 36);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(189, 15);
+            this.lblCustomerName.TabIndex = 0;
+            this.lblCustomerName.Text = "Tên khách hàng";
+            this.lblCustomerName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(3, 65);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(189, 15);
+            this.lblAddress.TabIndex = 0;
+            this.lblAddress.Text = "Địa chỉ khách hàng";
+            this.lblAddress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRemainder
+            // 
+            this.lblRemainder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblRemainder.AutoSize = true;
+            this.lblRemainder.Location = new System.Drawing.Point(3, 95);
+            this.lblRemainder.Name = "lblRemainder";
+            this.lblRemainder.Size = new System.Drawing.Size(189, 15);
+            this.lblRemainder.TabIndex = 0;
+            this.lblRemainder.Text = "Số dư ";
+            this.lblRemainder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAccountNumber
+            // 
+            this.txtAccountNumber.Location = new System.Drawing.Point(198, 3);
+            this.txtAccountNumber.Name = "txtAccountNumber";
+            this.txtAccountNumber.Size = new System.Drawing.Size(429, 23);
+            this.txtAccountNumber.TabIndex = 1;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(198, 32);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(429, 23);
+            this.txtCustomerName.TabIndex = 1;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(198, 61);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(429, 23);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtRemainder
+            // 
+            this.txtRemainder.Location = new System.Drawing.Point(198, 90);
+            this.txtRemainder.Name = "txtRemainder";
+            this.txtRemainder.Size = new System.Drawing.Size(429, 23);
+            this.txtRemainder.TabIndex = 1;
+            // 
+            // lvwInformation
+            // 
+            this.lvwInformation.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNumber,
+            this.colAccountNumber,
+            this.colCustomerName,
+            this.colAddress,
+            this.colRemainder});
+            this.lvwInformation.Location = new System.Drawing.Point(15, 351);
+            this.lvwInformation.Name = "lvwInformation";
+            this.lvwInformation.Size = new System.Drawing.Size(732, 198);
+            this.lvwInformation.TabIndex = 2;
+            this.lvwInformation.UseCompatibleStateImageBehavior = false;
+            this.lvwInformation.View = System.Windows.Forms.View.Details;
+            this.lvwInformation.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // colNumber
+            // 
+            this.colNumber.Text = "STT";
+            // 
+            // colAccountNumber
+            // 
+            this.colAccountNumber.Text = "Số tài khoản";
+            this.colAccountNumber.Width = 100;
+            // 
+            // colCustomerName
+            // 
+            this.colCustomerName.Text = "Tên khách hàng";
+            this.colCustomerName.Width = 100;
+            // 
+            // colAddress
+            // 
+            this.colAddress.Text = "Địa chỉ";
+            this.colAddress.Width = 200;
+            // 
+            // colRemainder
+            // 
+            this.colRemainder.Text = "Số tiền";
+            this.colRemainder.Width = 100;
+            // 
+            // btnAddUpdate
+            // 
+            this.btnAddUpdate.Location = new System.Drawing.Point(310, 290);
+            this.btnAddUpdate.Name = "btnAddUpdate";
+            this.btnAddUpdate.Size = new System.Drawing.Size(119, 29);
+            this.btnAddUpdate.TabIndex = 3;
+            this.btnAddUpdate.Text = "Thêm / Cập nhật";
+            this.btnAddUpdate.UseVisualStyleBackColor = true;
+            this.btnAddUpdate.Click += new System.EventHandler(this.btnAddUpdate_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(592, 290);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(81, 29);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Thoát";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(450, 290);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 29);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Xoá";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(821, 606);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddUpdate);
+            this.Controls.Add(this.lvwInformation);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tblTitle.ResumeLayout(false);
+            this.tblTitle.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tblTitle;
+        private Label lblTitle;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblAccountNumber;
+        private Label lblCustomerName;
+        private Label lblAddress;
+        private Label lblRemainder;
+        private TextBox txtAccountNumber;
+        private TextBox txtCustomerName;
+        private TextBox txtAddress;
+        private TextBox txtRemainder;
+        private ListView lvwInformation;
+        private ColumnHeader colNumber;
+        private ColumnHeader colAccountNumber;
+        private ColumnHeader colCustomerName;
+        private ColumnHeader colAddress;
+        private ColumnHeader colRemainder;
+        private Button btnAddUpdate;
+        private Button btnCancel;
+        private Button btnDelete;
+    }
+}
---
+++ AccountManager/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace AccountManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAccountNumber.Text) || string.IsNullOrEmpty(txtCustomerName.Text) || string.IsNullOrEmpty(txtCustomerName.Text) || string.IsNullOrEmpty(txtRemainder.Text))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin!!");
+            }
+            else
+            {
+                ListViewItem item = new ListViewItem();
+                for (int i = 1; i < lvwInformation.Items.Count + 2; i++)
+                {
+                    item.Text = i.ToString();
+                }
+                lvwInformation.Items.Add(item);
+                ListViewItem.ListViewSubItem subItem = new ListViewItem.ListViewSubItem(item, (txtAccountNumber.Text));
+                item.SubItems.Add(subItem);
+                subItem = new ListViewItem.ListViewSubItem(item, (txtCustomerName.Text));
+                item.SubItems.Add(subItem);
+                subItem = new ListViewItem.ListViewSubItem(item, (txtAddress.Text));
+                item.SubItems.Add(subItem);
+                subItem = new ListViewItem.ListViewSubItem(item, (txtRemainder.Text));
+                item.SubItems.Add(subItem);
+            }
+        }
+    }
+}
